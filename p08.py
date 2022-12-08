169a02841d7ef099109a1a40e328f6df20f25b6b
Menaka S <menakas.menakas@gmail.com>
2022-12-08 18:27:55 +0400

    shorten it further

--- p08.py
+++ p08.py
@@ -6,45 +6,37 @@ import pprint
 
 grid = []
 for line in sys.stdin:
-    line = line.strip()
-    grid.append([int(x) for x in line])
+    grid.append([int(x) for x in line.strip()])
 
 #pprint.pprint(grid)
 
+bounds = [0,len(grid)-1]
 
 def part1():
     visible = 0
     
     for i in range(0,len(grid)):
         for j in range(0,len(grid[i])):
-            if i ==0 or j == 0 or i == len(grid) - 1 or j == len(grid[i]) - 1:
-                visible +=1
-            else:
-                curr = grid[i][j]
-                right = max([x for x in grid[i][j+1:]])
-                left = max([x for x in grid[i][:j]])
-                below = max([grid[k][j] for k in range(i+1,len(grid))])
-                above = max([grid[k][j] for k in range(0,i)])
-                if right < curr or left < curr or below < curr or above < curr:
-                    visible +=1
+            visible += i in bounds or j in bounds or min(
+                        max([x for x in grid[i][j+1:]]),
+                        max([x for x in grid[i][:j]]),
+                        max([grid[k][j] for k in range(i+1,len(grid))]),
+                        max([grid[k][j] for k in range(0,i)])
+                    ) < grid[i][j]
     return (visible)
-
 
 def part2():
     mx = 0
    
     for i in range(0,len(grid)):
         for j in range(0,len(grid[i])):
-            left = right = above = below = ss = 0
-            if not (i ==0 or j == 0 or i == len(grid) - 1 or j == len(grid[i]) - 1):
-                right = min([k-j if grid[i][k] >= grid[i][j] else len(grid)-j-1 for k in range(j+1,len(grid))])
-                left = min([j-k if grid[i][k] >= grid[i][j] else j for k in range(j-1,-1,-1)])
-                below = min([k-i if grid[k][j] >= grid[i][j] else len(grid)-i-1 for k in range(i+1,len(grid))])
-                above = min([i-k if grid[k][j] >= grid[i][j] else i for k in range(i-1,-1,-1)])
-                ss = right * left * above * below
-                if ss > mx:
-                   mx = ss
-
+            if i not in bounds and j not in bounds:
+                mx = max(mx,
+                        min([k-j if grid[i][k] >= grid[i][j] else len(grid)-j-1 for k in range(j+1,len(grid))]) *
+                        min([j-k if grid[i][k] >= grid[i][j] else j for k in range(j-1,-1,-1)]) *
+                        min([k-i if grid[k][j] >= grid[i][j] else len(grid)-i-1 for k in range(i+1,len(grid))]) *
+                        min([i-k if grid[k][j] >= grid[i][j] else i for k in range(i-1,-1,-1)])
+                   )
     return (mx)
 
 print(part1())
